6837a9eb406fbef436085568ebbcc4ebf0f73585
Amit Singh <amitrajputdeonia00@gmail.com>
2025-03-27 18:06:46 +0530

    Time: 63 ms (14.23%), Space: 78.4 MB (8.3%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         // int maxi = *max_element(nums.begin(), nums.end());
-        unordered_map<int, int> mpp;
-        for(auto num : nums) mpp[num]++;
+        unordered_set<int> st;
+        for(auto num : nums) st.insert(num);
         for(int i = 1; i <= INT_MAX; i++) {
-            if(mpp.find(i) == mpp.end()) return i;
+            if(st.find(i) == st.end()) return i;
         }
 
         return 1;
